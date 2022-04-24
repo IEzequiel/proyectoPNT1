2c5a93d983ed28883ecbe9e1451b3e579c1d612f
sebastianRaiquenParisi <sebastianrparisi@gmail.com>
2022-04-24 18:15:05 -0300

    navbar se mueve!

--- carta-resto/javascript.js
+++ carta-resto/javascript.js
@@ -1,19 +1,22 @@
 //https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
 
-// Get the navbar
-var navbar = document.getElementById("navbar");
+document.addEventListener('DOMContentLoaded', function() {
+    // When the user scrolls the page, execute myFunction
+    window.onscroll = function() {myFunction()};
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+    // Get the navbar
+    var navbar = document.getElementById("navbar");
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+    // Get the offset position of the navbar
+    var sticky = navbar.offsetTop;
+
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    }
+  })
