3bbf56ac86099551d80d96bad98d0532418b370a
Ezequiel Ilivitzky <ezequiel.ilivitzky@ort.edu.ar>
2022-05-06 15:34:56 -0300

    se agrega validacion de mail y telefono por JS

--- carta-resto/formValidations.js
+++ carta-resto/formValidations.js
@@ -1,11 +1,11 @@
 function required()
 {
 let nomb = document.forms["formulario"]["nombre"].value;
-var apell = document.forms["formulario"]["apellido"].value;
-var email = document.forms["formulario"]["email"].value;
-var tel = document.forms["formulario"]["tel"].value;
-var dir = document.forms["formulario"]["dir"].value;
-var pedido = document.forms["formulario"]["pedido"].value;
+let apell = document.forms["formulario"]["apellido"].value;
+let email = document.forms["formulario"]["email"].value;
+let tel = document.forms["formulario"]["tel"].value;
+let dir = document.forms["formulario"]["dir"].value;
+let pedido = document.forms["formulario"]["pedido"].value;
 if (nomb == "")
 {
 alert("Ingrese nombre");
@@ -13,26 +13,58 @@ return false;
     } else if (apell == "") {
     alert("Ingrese Apellido")
     return false;
-        } else if (email == "") {
-            alert("Ingrese email")
-            return false;
-                }
-    else if (tel == "") {
-        alert("Ingrese telefono")
-        return false;
         }
-        else if (dir == "") {
-            alert("Ingrese direccion")
+        else if (email == "") {
+            alert("Ingrese mail")
             return false;
-            }
-            else if (pedido == "") {
-                alert("Ingrese pedido")
+             } 
+            else if (tel == "") {
+            alert("Ingrese telefono")
+            return false;
+             }
+              else if (dir == "") {
+                alert("Ingrese direccion")
                 return false;
-                }
-                    else 
-                    {
-                     alert("Pedido realizado");
-                     return true; 
-                     }
+                    }
+                    else if (pedido == "") {
+                        alert("Ingrese pedido")
+                        return false;
+                        }
+                        else 
+                          {
+                            if (ValidarMail(document.formulario.email) && ValidarTelefono(document.formulario.tel)){
+                            alert("Pedido realizado");
+                            }
+                          }
 }
 
+function ValidarMail(inputText)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+document.formulario.email.focus();
+return true; 
+}
+else
+{
+alert("Ingresaste un mail incorrecto!");
+document.formulario.email.focus();
+return false;
+}
+}
+
+function ValidarTelefono(inputtxt)
+{
+  var phoneno = /^\+?([0-9]{3})\)?[-. ]?([0-9]{8})$/;
+  if(inputtxt.value.match(phoneno))
+        {
+      
+      return true;
+        }
+      else
+        {
+        alert("Numero invalido.Ej:(011)-12345678");
+        return false;
+        }
+}
