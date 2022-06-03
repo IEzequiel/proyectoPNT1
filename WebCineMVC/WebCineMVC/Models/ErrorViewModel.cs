61c9e3efa62e525eebf2e149507549c792d86592
sebastianRaiquenParisi <sebastianrparisi@gmail.com>
2022-06-03 20:44:21 -0300

    Modelos terminados y bd hecha

--- WebCineMVC/WebCineMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebCineMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
